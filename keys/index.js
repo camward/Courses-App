0ca1532dc1a3a91b00f67e429284125c34a1176c
alex <camward@yandex.ru>
2019-10-28 20:25:34 +0400

    отправка письма npm i connect-flash connect-mongodb-session

--- keys/index.js
+++ keys/index.js
@@ -1,5 +1,7 @@
 module.exports = {
   MONGODB_URI: 'mongodb+srv://alex:XKn2hdqbw3XLBUyI@cluster0-ogayr.mongodb.net/test?retryWrites=true&w=majority',
   SESSION_SECRET: 'some secret value',
-  SENDGRID_API_KEY: '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'
+  SENDGRID_API_KEY: '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',
+  EMAIL_FROM: 'nodejs@email.ru',
+  BASE_URL: 'http://localhost:3000'
 }
