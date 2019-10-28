684f34796cfb314b92db41d777dac8d6098a37d3
alex <camward@yandex.ru>
2019-10-28 19:57:36 +0400

    настройка почтового сервиса https://sendgrid.com/

--- keys/index.js
+++ keys/index.js
@@ -1,4 +1,5 @@
 module.exports = {
   MONGODB_URI: 'mongodb+srv://alex:XKn2hdqbw3XLBUyI@cluster0-ogayr.mongodb.net/test?retryWrites=true&w=majority',
-  SESSION_SECRET: 'some secret value'
+  SESSION_SECRET: 'some secret value',
+  SENDGRID_API_KEY: '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'
 }
